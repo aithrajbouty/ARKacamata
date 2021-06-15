e0b28230cd1a031bd5fc93c63f7222aad363355f
aithrajbouty <aithrabouty@gmail.com>
2021-06-15 22:39:55 +0800

    ubah halaman awal + tombol play

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,12 +7,12 @@ public class MainMenu : MonoBehaviour
 {
     public void InformationMenu()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     public void backToMainMenu()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 }
